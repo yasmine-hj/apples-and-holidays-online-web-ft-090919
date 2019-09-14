e9bca0ad202ddb30409c2e6a55d3cfe71a81e937
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-14 11:50:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.map do 
+holiday_hash.map do |seasons, holiday|
 end
 
 
